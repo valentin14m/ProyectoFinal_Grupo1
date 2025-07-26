e2289072e9c7f50050e20d45b50be52d33dae078
José Valentín Medrano Páramo <vmedrano14@MacBook-Pro-de-Valentin.local>
2025-07-26 11:26:38 -0600

    Iniciando proyecto Java

---
+++ src/main/java/org/yourcompany/yourproject/ProyectoFinal_Grupo1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author vmedrano14
+ */
+public class ProyectoFinal_Grupo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
